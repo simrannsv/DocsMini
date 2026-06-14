a4f8a18a1483fd0643a061279404504c00e01e3f
Simran Varthavani <simssV08@gmail.com>
2026-06-14 19:08:41 +0530

    Update API base URL to use environment variable

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:4545',
+  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:4545',
   withCredentials: true
 })
 
